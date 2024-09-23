c6dc2527a5d40fc766d009f9b33069e209c44567
Vincent Chen <grass527@gmail.com>
2024-09-24 00:20:00 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/web-mmo-client/', // 設定為 GitHub Pages 的路徑
+  base: '/web-mmo/', // 設定為 GitHub Pages 的路徑
   // 其他配置...
 });
